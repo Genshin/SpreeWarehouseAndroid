75c9c34e306a5de573764183c4685e59cd377d7c
影月 零 <zero@genshin.org>
2012-02-26 13:10:25 +0900

    initial commit

---
+++ src/org/genshin/warehouse/WarehouseActivity.java
@@ -0,0 +1,13 @@
+package org.genshin.warehouse;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class WarehouseActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
