803606066c6fd10fad94e5bfedc349fc85e790bd
影月 零 <zero@genshin.org>
2012-05-05 19:03:11 +0900

    temp commit

--- src/org/genshin/warehouse/products/ProductEditActivity.java
+++ src/org/genshin/warehouse/products/ProductEditActivity.java
@@ -1,7 +1,19 @@
 package org.genshin.warehouse.products;
 
 import android.app.Activity;
+import android.os.Bundle;
+
+import org.genshin.warehouse.R;
+import org.genshin.warehouse.products.Product;
 
 public class ProductEditActivity extends Activity {
+	private Product product;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.product_edit);
+	}
+
 
 }
